d5150dd699a20230aec4cc3da5c4b10ec9316a8b
Andrei Karushev <infodusha@ya.ru>
2025-03-02 00:05:48 +0100

    fix: commit of shame
    JUST IN CASE

--- functions/api/[method].ts
+++ functions/api/[method].ts
@@ -4,5 +4,7 @@ export const onRequest: PagesFunction = async (context) => {
   const method = context.params.method as string;
   const url = new URL(method, API_URL);
   const request = new Request(url, context.request);
+  request.headers.set('Origin', API_URL);
+  request.headers.set('Referer', API_URL);
   return await fetch(request);
 };
