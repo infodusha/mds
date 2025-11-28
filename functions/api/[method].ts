66aa2fd595082e667a4b0cfca126e22b1ad62962
Andrei Karushev <infodusha@ya.ru>
2025-11-28 17:01:51 +0100

    fix: api

--- functions/api/[method].ts
+++ functions/api/[method].ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://mds-online.ru/';
+const API_URL = 'https://mds-old.ru/';
 
 export const onRequest: PagesFunction = async (context) => {
   const method = context.params.method as string;
@@ -6,12 +6,5 @@ export const onRequest: PagesFunction = async (context) => {
   const request = new Request(url, context.request);
   request.headers.set('Origin', API_URL);
   request.headers.set('Referer', API_URL);
-  const response = await fetch(request);
-  const headers = new Headers(response.headers);
-  headers.set('Access-Control-Allow-Origin', 'https://mds.infodusha.ru');
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: headers,
-  });
+  return fetch(request);
 };
